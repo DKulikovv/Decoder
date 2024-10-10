4acbd78feac126ec5751ef7436ebebc7510280a9
Kulikov Dmitry <Dmitry.Kulikov@swtecnn.com>
2024-10-10 14:35:13 +0300

    Added Chardet library
    Signed-off-by: Kulikov Dmitry <Dmitry.Kulikov@swtecnn.com>

---
+++ Chardet_Library/src/nscore.h
@@ -0,0 +1,12 @@
+#ifndef INCLUDED_NSCORE_H
+#define INCLUDED_NSCORE_H
+
+#define PR_TRUE 1
+#define PR_FALSE 0
+
+#define nsnull 0
+
+#define NS_OK 0
+#define NS_ERROR_OUT_OF_MEMORY ((unsigned)(0x8007000eL))
+
+#endif /* INCLUDED_NSCORE_H */
