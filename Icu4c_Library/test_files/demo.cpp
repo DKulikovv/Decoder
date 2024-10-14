7818e51c830b8e168e5b7e47cfee51586afb3290
Kulikov Dmitry <Dmitry.Kulikov@swtecnn.com>
2024-10-14 17:05:49 +0300

    cleanup icu4c
    Signed-off-by: Kulikov Dmitry <Dmitry.Kulikov@swtecnn.com>

--- Icu4c_Library/test_files/demo.cpp
+++ Icu4c_Library/test_files/demo.cpp
@@ -1,15 +1,5 @@
 #include "../src/general_decoder.hpp"
 
 int main() {
-
-  ICU_Decoder d;
-
-  d.Decode("../test_files/big5.txt");
-  d.Decode("../test_files/iso-8859-5.txt");
-  d.Decode("../test_files/koi-8r.txt");
-  d.Decode("../test_files/utf-8.txt");
-  d.Decode("../test_files/utf-16.txt");
-  d.Decode("../test_files/utf-32.txt");
-  d.Decode("../test_files/win1251.txt");
   return 0;
 }
